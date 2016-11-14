81f44356c64f43f5ca86794b911586fd229e7efe
Stuart Updegrave <stuart@updegrave.com>
2016-11-14 16:41:59 +0100

    initial commit: base app + storage service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StorageService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'fnord';
+
+  constructor(private storagesvc: StorageService) {
+  }
 }
